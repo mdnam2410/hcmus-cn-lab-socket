89f457788188df08a9ba86bc0f6b3185a728e24e
Nam Mai <mdnam2410@gmail.com>
2021-05-06 11:55:08 +0700

    Add database.py

---
+++ database.py
@@ -0,0 +1,212 @@
+import datetime
+import sqlite3
+import time
+
+class DatabaseConnectionError(Exception):
+    pass
+
+class Database:
+    """Class that provides encapsulation for basic queries needed for the app"""
+
+    def __init__(self, database_path):
+        self.today = datetime.date.today()
+        self.con = None
+        try:
+            self.con = sqlite3.connect(database_path)
+            self.con.row_factory = sqlite3.Row
+            self.cur = self.con.cursor()
+        except sqlite3.Error:
+            raise DatabaseConnectionError(f'Cannot connect to {database_path}')
+    
+    def __del__(self):
+        self.con.close()
+
+    def execute_query(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def commit(self):
+        self.con.commit()
+
+    # User and admin methods
+
+    def authenticate(self, username, password):
+        """Authenticate a user with given username and password
+        
+        Parameters
+        ----------
+        username : str
+        password : str
+
+        Returns
+        -------
+        list
+            A list of (username, name) (maximum one element).
+
+        """
+
+        query = """
+        SELECT user.username, user.name
+        FROM user
+        WHERE user.username = ? AND user.password = ?;
+        """
+        self.cur.execute(query, (username, password))
+        return self.cur.fetchall()
+
+    def sign_up(self, username, password, name) -> bool:
+        """Register a user
+
+        Parameters
+        ----------
+        username : str
+        password : str
+        name : str
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+        """
+
+        # Check if username already existed
+        self.cur.execute("SELECT username FROM user WHERE username = ?", (username,))
+        r = self.cur.fetchall()
+        if len(r) != 0:
+            return False
+        else:
+            query = """
+            INSERT INTO user VALUES (?, ?, ?);
+            """
+            self.cur.execute(query, (username, password, name))
+            self.con.commit()
+            return True
+
+    # Weather query methods
+
+    def weather_by_date(self, date: str):
+        """Retrieve weather condition of all cities in a given date
+
+        Parameters
+        ----------
+        date : str
+            In ISO 8601 format (YYYY-MM-DD).
+
+        Returns
+        -------
+        list
+            A list of (city_id, city_name, country_name, report_date, main, min_degree, max_degree, precipitaion).
+        """
+
+        query = """
+        SELECT c.city_id, c.city_name, ct.country_name, cw.report_date, 
+               wc.main, cw.min_degree, cw.max_degree, cw.precipitation
+        FROM   city_weather AS cw JOIN city AS c
+               ON cw.city_id = c.city_id
+               JOIN weather_condition AS wc
+               ON cw.weather_id = wc.weather_id
+               JOIN country AS ct
+               ON c.country_code = ct.country_code
+        WHERE  cw.report_date = ?;
+        """
+        self.cur.execute(query, (date,))
+        return self.cur.fetchall()
+
+    def today_weather(self):
+        """Retrieve today weather information of all cities
+
+        Returns
+        -------
+        list
+            A list of (city_id, city_name, country_name, report_date, main, min_degree, max_degree, precipitaion).
+        """
+
+        return self.weather_by_date(self.today.isoformat())
+
+    def weather_by_date_range(self, city_id, start, end):
+        """Retrieve weather information of a city in a date range [start, end]
+
+        Parameters
+        ----------
+        city_id : int
+            ID of the city to query.
+        start : str
+            Start date, in ISO 8601 format (YYYY-MM-DD).
+        end : str
+            End date, in ISO 8601 format (YYYY-MM-DD), required that start <= end
+
+        Returns
+        -------
+        list
+            A list of (city_id, city_name, country_name, report_date, main, min_degree, max_degree, precipitaion).
+        None
+            Returns if start > date
+        """
+
+        query = """
+        SELECT c.city_id, c.city_name, ct.country_name, cw.report_date, 
+               wc.main, cw.min_degree, cw.max_degree, cw.precipitation
+        FROM   city_weather AS cw JOIN city AS c
+               ON cw.city_id = c.city_id
+               JOIN weather_condition AS wc
+               ON cw.weather_id = wc.weather_id
+               JOIN country AS ct
+               ON c.country_code = ct.country_code
+        WHERE  c.city_id = ? AND cw.report_date BETWEEN ? AND ?;"""
+
+        s = datetime.date.fromisoformat(start)
+        e = datetime.date.fromisoformat(end)
+        if s <= e:
+            self.cur.execute(query, (city_id, start, end))
+            return self.cur.fetchall()
+        else:
+            return None
+
+    def forecast(self, city_id):
+        """Retrieve 7-day weather forecast information for a given city
+        
+        Parameters
+        ----------
+        city_id : int
+
+        Returns
+        -------
+        list
+            A list of (city_id, city_name, country_name, report_date, main, min_degree, max_degree, precipitaion),
+            sorted by report_date
+        """
+
+        return self.weather_by_date_range(
+            city_id,
+            start=self.today.isoformat(),
+            end=(self.today + datetime.timedelta(days=6)).isoformat()
+        )
+
+    # Searching
+
+    def search_city(self, name):
+        """Search city by name
+        
+        Parameters
+        ----------
+        name : str
+
+        Returns
+        -------
+        list
+            A list of (city_id, city_name, country_name).
+        """
+
+        query = """
+        SELECT c.city_id, c.city_name, ct.country_name
+        FROM city AS c JOIN country AS ct ON c.country_code = ct.country_code
+        WHERE city_name LIKE ?;
+        """
+        name = name.lower()
+        name = '%' + name + '%'
+        self.cur.execute(query, (name,))
+        return self.cur.fetchall()
+
+
+if __name__ == '__main__':
+    db = Database('db/wether.db')
+
