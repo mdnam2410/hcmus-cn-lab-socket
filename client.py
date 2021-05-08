fa2b4ba6fcbc54f719076330417aa79521433d26
Nam Mai <mdnam2410@gmail.com>
2021-05-08 10:39:43 +0700

    Add base class App and rewrite Client and Server

--- client.py
+++ client.py
@@ -1,83 +1,62 @@
+import app
 import util
 
 import socket
 import time
 
-DISCOVERY_PORT = 2410
-SERVER_PORT = 2802
-
-def discover_server() -> socket.socket:
-    """ This function mimics the behavior of a DNS client when it tries to
-    find a DNS server on the same network. The client broadcasts a discovery message
-    into the network using UDP, and if the server receives, it will reply with an
-    acknowledgement message along with its IP address. The client then tries to create 
-    a TCP connection to this address.
-    If the protocol is carried out successfully, the function returns a new socket
-    object for the newly-created TCP connection. Otherwise, None is returned.
-    """
-
-    # Create an UDP socket
-    u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Broadcast the discovery message
-    u.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    u.sendto(util.package('discover', '', ''), ('255.255.255.255', DISCOVERY_PORT))
-    
-    # Receive the acknowledgement message from the server
-    message, _ = u.recvfrom(1024)
-    util.print_message(message)
-
-    # Extract the acknowledgement message
-    e, _, _, addr = util.extract(message)
-
-    if e == '000':
-        # The UDP socket is completed
-        u.close()
-
-        # Create new TCP connection using the extracted server address
-        t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            t.connect((addr, SERVER_PORT))
-            return t
-        except ConnectionRefusedError:
-            return None
-    else:
-        return None
-
 class ConnectionError(Exception):
     pass
 
-class Client:
+class Client(app.App):
     def __init__(self):
-        self.s = discover_server()
-        if self.s is None:
+        super().__init__()
+
+        self.main_socket = self.discover_server()
+        if self.main_socket is None:
             raise ConnectionError('Unable to connect to server')
     
-    def send(self, message):
-        self.s.send(message)
+    def discover_server(self) -> socket.socket:
+        """ This function mimics the behavior of a DNS client when it tries to
+        find a DNS server on the same network. The client broadcasts a discovery message
+        into the network using UDP, and if the server receives, it will reply with an
+        acknowledgement message along with its IP address. The client then tries to create 
+        a TCP connection to this address.
+        If the protocol is carried out successfully, the function returns a new socket
+        object for the newly-created TCP connection. Otherwise, None is returned.
+        """
 
-    def receive(self):
-        message = self.s.recv(2 ** 16)
-        return message
+        # Create an UDP socket
+        u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Broadcast the discovery message
+        u.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        u.sendto(util.package('discover', '', ''), ('255.255.255.255', self.DISCOVERY_PORT))
+        
+        # Receive the acknowledgement message from the server
+        message, _ = u.recvfrom(1024)
+        util.print_message(message)
+
+        # Extract the acknowledgement message
+        e, _, _, addr = util.extract(message)
+
+        if e == '000':
+            # The UDP socket is completed
+            u.close()
+
+            # Create new TCP connection using the extracted server address
+            t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                t.connect((addr, self.SERVER_PORT))
+                return t
+            except ConnectionRefusedError:
+                return None
+        else:
+            return None
     
-    def receive2(self):
-        self.socket_buffer = 1024
-
-        message = self.s.recv(self.socket_buffer)
-        _, _, size, _ = util.extract(message)
-
-        # The number of the rest submessages need to receive
-        r = size // self.socket_buffer - 1
-        print(r)
-        while r >= 0:
-            message += self.s.recv(self.socket_buffer)
-            r -= 1
-        return message
-
     def test(self):
         data = ''
-        self.send(util.package('kuso', '', data))
-        m = self.receive2()
+        self.send(util.package('test', '', data))
+        m = self.receive()
         status_code, status_message, size, data = util.extract(m)
         print(size)
         print(len(data))
@@ -86,12 +65,11 @@ class Client:
         self.username = input('Username: ')
         self.password = input('Password: ')
         self.send(util.package('login', '', f'{self.username},{self.password}'))
-        status_code, status_message, _ = util.extract(self.receive())
+        status_code, status_message, *_ = util.extract(self.receive())
         if status_code == 0:
             print('Logged in')
         else:
             print('Error:', status_message)
 
 client = Client()
-# client.login()
-client.test()
+client.login()
--- server.py
+++ server.py
@@ -1,75 +1,86 @@
+import app
 import database
 import util
 
 import socket
 import threading
 
-SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
-SERVER_PORT = 2802
-DISCOVERY_PORT = 2410
-
-def response_to_discovery_request():
-    """This function listens for remote client discovery message and responses
-    with the server address"""
-
-    # Create the UDP discovery socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('', DISCOVERY_PORT))
-
-    while True:
-        # Listen for requests
-        message, client_connection = s.recvfrom(1024)
-        util.print_message(message)
-
-        # Verify the discovery message
-        command, *_ = util.extract(message)
-        if command == 'discover':
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            s.sendto(
-                util.package('000', '', SERVER_ADDRESS),
-                ('255.255.255.255', client_connection[1])
-            )
-
-class Server:
+class Server(app.App):
     def __init__(self):
-        self.serving_functions = {
-            'login': self.command_login,
-            'kuso': self.kuso
-        }
+        super().__init__()
 
+        self.SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
+
+        # Main database
+        self.db = database.Database('db/wether.db')
+
+        # Dictionary to translate status codes to status messages
         self.status_messages = {
             '000': 'OK',
             '100': 'Username or password not found'
         }
 
-        self.db = database.Database('db/wether.db')
+        self.requests = {
+            'test': self.test,
+            'login': self.request_login
+        }
+
+        self.main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.main_socket.bind((self.SERVER_ADDRESS, self.SERVER_PORT))
 
         # Background thread that listens and responses to discovery requests from remote clients
-        self.thread_discovery = threading.Thread(target=response_to_discovery_request, daemon=True)
-        self.thread_discovery.start()
+        self.thread_discovery = threading.Thread(target=self.response_to_discovery_request, daemon=True)
+    
+    def response_to_discovery_request(self):
+        """This function listens for remote client discovery message and responses
+        with the server address"""
 
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((SERVER_ADDRESS, SERVER_PORT))
-        self.server_socket.listen()
+        # Create the UDP discovery socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('', self.DISCOVERY_PORT))
 
         while True:
-            c, _ = self.server_socket.accept()
-            m = c.recv(1024)
-            command, option, _, data = util.extract(m)
-            t = self.serving_functions[command](option, data)
-            reply = util.package(t[0], self.status_messages[t[0]], t[1])
-            # util.print_message(reply)
-            c.send(reply)
-        
-    def command_login(self, option, data):
+            # Listen for requests
+            message, client_connection = s.recvfrom(1024)
+            # util.print_message(message)
+
+            # Verify the discovery message
+            command, *_ = util.extract(message)
+            if command == 'discover':
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                s.sendto(
+                    util.package('000', '', self.SERVER_ADDRESS),
+                    ('255.255.255.255', client_connection[1])
+                )
+    
+    def run(self):
+        self.main_socket.listen()
+        self.thread_discovery.start()
+
+        while True:
+            # Accept connection from clients
+            conn, _ = self.main_socket.accept()
+
+            # Extract request message
+            m = self.receive_from(conn)
+            command, command_type, _, data = util.extract(m)
+
+            # Do the request
+            t = self.requests[command](command_type, data)
+
+            # Response
+            response = util.package(t[0], self.status_messages[t[0]], t[1])
+            conn.send(response)
+    
+    def test(self, command_type, data):
+        return ('000', 'vl' * 1000)
+
+    def request_login(self, command_type, data):
         username, password = data.split(',', 1)
         result = self.db.authenticate(username, password)
         if len(result) == 1:
             return ('000', f'{username},{password},{result[0][2]}')
         return ('100', '')
 
-    def kuso(self, command_type, data):
-        return ('000', 'kusotare' * 1024)
-
-
-server = Server()
+s = Server()
+s.run()
